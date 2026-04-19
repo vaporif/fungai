0b7e4f97f05ea6992c5c00f1a5537bda632e0ccf
Dmytro Onypko <vaporif@proton.me>
2026-04-19 16:57:16 +0100

    project structure

---
+++ crates/shroom_input/src/lib.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+pub struct InputPlugin;
+
+impl Plugin for InputPlugin {
+    fn build(&self, _app: &mut App) {}
+}
--- crates/shroom_shrooms/src/main.rs
+++ crates/shroom_shrooms/src/main.rs
@@ -1,5 +1,28 @@
 use bevy::prelude::*;
 
+use shroom_ai::AiPlugin;
+use shroom_core::CorePlugin;
+use shroom_fruiting::FruitingPlugin;
+use shroom_growth::GrowthPlugin;
+use shroom_input::InputPlugin;
+use shroom_regions::RegionsPlugin;
+use shroom_render::RenderPlugin;
+use shroom_ui::UiPlugin;
+use shroom_world::WorldPlugin;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            CorePlugin,
+            WorldPlugin,
+            GrowthPlugin,
+            RegionsPlugin,
+            RenderPlugin,
+            InputPlugin,
+            AiPlugin,
+            FruitingPlugin,
+            UiPlugin,
+        ))
+        .run();
 }
